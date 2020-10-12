84dc6f58779c473d735bf0ffce953c215b756e08
Reinis Vate <reinis.vate@emergn.com>
2020-10-12 21:04:56 +0300

    fix: fix condition

--- 3rd-lecture-3rd-task.py
+++ 3rd-lecture-3rd-task.py
@@ -3,14 +3,14 @@ a = input('a: ')
 b = input('b: ')
 c = input('c: ')
 
-if a<b and a<c:
-    if b<c: print(f"a {a}, b {b}, c {c}")
+if a<=b and a<=c:
+    if b<=c: print(f"a {a}, b {b}, c {c}")
     else: print(f"a {a}, c {c}, b {b}")
-elif b<a and b<c:
-    if a<c: print(f"b {b}, a {a}, c {c}")
+elif b<=a and b<=c:
+    if a<=c: print(f"b {b}, a {a}, c {c}")
     else: print (f"b {b}, c {c}, a {a}")
-else c<a and c<b:
-    if a<b: print(f"c {c}, a {a}, b {b}")
+else:
+    if a<=b: print(f"c {c}, a {a}, b {b}")
     else: print (f"c {c}, b {b}, a {a}")
 
 #other option (easier)
