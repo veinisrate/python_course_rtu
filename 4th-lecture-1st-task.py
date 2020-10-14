45e99ddcf4b80c861058ca900ba60e39bd796b7d
Reinis Vate <reinis.vate@emergn.com>
2020-10-14 20:35:01 +0300

    fix: fizzbuzz

--- 4th-lecture-1st-task.py
+++ 4th-lecture-1st-task.py
@@ -2,11 +2,13 @@ num = 1
 end = 100
 while num <= end:
     if num%5==0 and num%7==0:
-        print("FizzBuzz", end=",")
+        print("FizzBuzz", end="")
     elif num%5==0:
-        print("Fizz", end=",")
+        print("Fizz", end="")
     elif num%7==0:
-        print("Buzz", end=",")
+        print("Buzz", end="")
     else:
-        print(str(num), end=",")
+        print(str(num), end="")
+    if num!=end:
+        print(",", end="")
     num+=1
