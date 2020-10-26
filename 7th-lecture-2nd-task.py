0515c3844e80394dd54efcff9d186b3f83646646
Reinis Vate <reinis.vate@emergn.com>
2020-10-26 20:06:14 +0200

    fix: refactor 1st task

--- 7th-lecture-2nd-task.py
+++ 7th-lecture-2nd-task.py
@@ -3,20 +3,20 @@
 # kas atgriež bool True vai False atkarībā vai vārds vai teikums ir lasāms vienādi no abām pusēm.
 # PS no sākuma varat sākt ar viena vārda risinājumu, bet pilns risinājums ignorēs atstarpes(whitespace) un lielos/mazos burtus
 # is_palindrome("Alus ari ira      sula") -> True
-
 def is_palindrome(text:str) -> bool:
     textNoExtraSpaces = " ".join(text.split())
     text_list = text.split()
     text_list.reverse()
+
     reverseSentence_list = []
     for word in text_list:
-        temp = word[::-1]
-        reverseSentence_list.append("".join(temp))
-    if textNoExtraSpaces == " ".join(reverseSentence_list):
-        return True
-    else:
-        return False
+        reverseSentence_list.append("".join(word[::-1]))
 
+    if textNoExtraSpaces == " ".join(reverseSentence_list): return True
+    else: return False
+
+
+#tests
 print(is_palindrome("alus    ari ira sula"))
 print(is_palindrome("sula"))
 print(is_palindrome("ala"))
